52092d14d63f373ae255fce5a2db59eb446233fe
reggyshicky <reginahshikanda@gmail.com>
2023-07-04 14:55:11 +0300

    ruby regex

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,10 +1,2 @@
 #!/usr/bin/env/ ruby
-# hb matches the literal characters hb at the beginning
-# (t{2,5})captures the letter t repeated 2 to 5 times using a quantifier
-# {2, 5} and n matches the n at the end
-# The script prints the matchss seperated by \n
-
-pattern /hb(t{2,5})n/
-text = ARGV[0]
-matches = text.scan(pattern)
-puts matches.join("\n")
+puts ARGV[0].scan(/hbt{2,4}n/).join
