170c80f010b1b52768b23712060e0f4c56e55cfe
reggyshicky <reginahshikanda@gmail.com>
2023-08-27 19:50:11 +0300

    SSH client configuration must be configured to refuse to authenticate using a password

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,17 +3,17 @@
 include stdlib
 
 file_line { 'SSH Private Key':
-  path => '/etc/ssh/ssh_config',
-  line => '    IdentityFile ~/.ssh/schhol',
-  match => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
-  replace => true,
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/schhol',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
   append_on_no_match => true,
 }
 
 file_line { 'Deny Password Auth':
-  path => '/etc/ssh/ssh_config',
-  line => '    PasswordAuthentication no'
-  match  => '^[#]+[\s]*(?!)PasswordAuthentication[\s]+(yes|no)$',
-  replace => true,
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no'
+  match              => '^[#]+[\s]*(?!)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
   append_on_no_match => true
 }
