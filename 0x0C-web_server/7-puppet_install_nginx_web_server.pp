8a0c6c20bdbf68013d765c3cb19d3cf1e04864f1
reggyshicky <reginahshikanda@gmail.com>
2023-08-29 17:08:26 +0300

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -11,8 +11,8 @@ file { '/var/www/html/index.html':
 file_line { 'redirection-301':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;'
-  line   => 'rewrite ^/redirect_me https://github.com permanent;'
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
 }
 
 service { 'nginx':
