271ec33c8f1fb967fdf74cc9b4c4bb65fd4215cd
reggyshicky <reginahshikanda@gmail.com>
2023-09-25 16:45:20 +0300

    retrieves done tasks using REST API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+returns info about employees TODO list progress using
+RESTAPI
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users/"
+    user = requests.get(url + "{}".format(argv[1])).json()
+    todos = requests.get(url + "{}/todos".format(argv[1])).json()
+    tasks = [todo.get('title') for todo in todos
+             if todo.get('completed') is True]
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user.get('name'), len(tasks), len(todos)))
+
+    for task in tasks:
+        print("\t {}".format(task))
