eb94277fe5c595a57a0ddb7038161ece24ff3743
reggyshicky <reginahshikanda@gmail.com>
2023-10-10 15:53:22 +0300

    fetches top ten hot posts of a subredit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module that queries the Reddit API and prints the titles of the
+first 1O hot posts for a given subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+    headers = {'User-Agent': 'Reggy'}
+
+    try:
+        res = requests.get(url, headers=headers,
+                           allow_redirects=False)
+        if res.status_code == 200:
+            # children is var that will hold a list of dicts representing
+            # the child elements of a Reddit API response
+            # extracts the children field fro the json response returned
+            # by thev API. The children field typically contain the info
+            # about the posts
+            posts = res.json().get('data').get('children')
+            for i in range(10):
+                print(posts[i].get('data').get('title'))
+        else:
+            print("None")
+    except Exception:
+        print("None")
