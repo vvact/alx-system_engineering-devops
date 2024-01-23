651a27a8d8f47f51beb7cf8cfd0d6fc3b509434d
Victor Midega <vbm7752@gmail.com>
2024-01-23 03:46:42 -0800

    new commit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-# Install Flask version 2.1.0 using pip3
+# installs flask
+
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+    ensure   => '2.1.0',
+    provider => 'pip3'
 }
 
